f231b7e9b0dc68b5f15ba0d15c45b02f3f3226ef
Mediacom99 <bertoliedoardo99@gmail.com>
2024-02-06 02:06:46 +0100

    Neatty coddy

--- atmega328p-keypad/src/main.rs
+++ atmega328p-keypad/src/main.rs
@@ -5,57 +5,20 @@
 *   4. open that device
 *   5. Do smth using HidDevice (read write)
 * */
-use rusb::*;
+use rusb::Context;
 mod rusbutils;
-use std::process::exit;
-use std::time::Duration;
-
-const TIMEOUT: Duration = Duration::from_secs(60);
-
-const VID: u16 = 0x16c0;
-const PID: u16 = 0x05dc;
-const MAXBUFSIZE: usize = 1024;
-//const VID: u16 = 3599;
-//const PID: u16 = 3;
-//const PAYLOAD_BYTES: usize = 128;
-//const REPORT_BYTES: usize = 8;
 
 fn main() {
     //Create new libusb context
     let context = Context::new().expect("Could not create a libusb context!");
 
-    //list_usb_devices(&context);
+    //List all usb devices, get Manuf and Prod ID, Max Packet size and Num of config descriptor
+    //Since in order to read the ID's I have to open a device and silly Windows does not let me do
+    //that if that device is either not HID or using WinUSB or some other windows driver...
+    //So for example you will not get any information on a Focusrite for example, even though
+    //it's using the USB protocol...
+    // rusbutils::list_usb_devices(&context);
 
-    //Open and get a handle to a device with a certain VID and PID
-    let dhandle = match context.open_device_with_vid_pid(VID, PID) {
-        Some(res) => res,
-        None => {
-            println!("Could not open device");
-            exit(1);
-        }
-    };
-
-    //Hid request protocol
-    let request_type: u8 = 0xa1;
-    let request: u8 = 0x01;
-    let value: u16 = 0x00;
-    let index: u16 = 0x00;
-    let mut buffer: [u8; MAXBUFSIZE] = [0; MAXBUFSIZE];
-
-    loop {
-        //First send an input request to the device then read whatever the device sends back
-        let bytes_read: usize =
-            match dhandle.read_control(request_type, request, value, index, &mut buffer, TIMEOUT) {
-                Ok(br) => br,
-                Err(err) => {
-                    println!("No bytes read! Error: {}", err);
-                    exit(2);
-                }
-            };
-        println!("Bytes read: {bytes_read}");
-        //lossy will turn every valid utf8 character into the appropriate symbol, while the invalid
-        //ones will be shown as this symbol: � (it returns a smart pointer)
-        let from_device = String::from_utf8_lossy(&buffer);
-        println!("From device: {}", from_device);
-    }
+    //Read bytes from the micro, turn them into a utf8 string if possible and print it out
+    rusbutils::read_from_micro(&context);
 }
--- atmega328p-keypad/src/rusbutils.rs
+++ atmega328p-keypad/src/rusbutils.rs
@@ -1,5 +1,16 @@
 use rusb::*;
 use std::process::exit;
+use std::time::Duration;
+
+const TIMEOUT: Duration = Duration::from_secs(60);
+
+const VID: u16 = 0x16c0;
+const PID: u16 = 0x05dc;
+const MAXBUFSIZE: usize = 1024;
+//const VID: u16 = 3599;
+//const PID: u16 = 3;
+//const PAYLOAD_BYTES: usize = 128;
+//const REPORT_BYTES: usize = 8;
 
 struct DeviceDescriptor {
     vid: u16,        //Vendor id
@@ -19,6 +30,43 @@ impl DeviceDescriptor {
         println!("Max packet size: {}B", self.mps);
         println!("Number of config descriptors: {}", self.ncd);
         println!("\n");
+    }
+}
+
+//Read bytes from the device using a control transfer type
+pub fn read_from_micro(context: &Context) {
+    //Open and get a handle to a device with a certain VID and PID
+    let dhandle = match context.open_device_with_vid_pid(VID, PID) {
+        Some(res) => res,
+        None => {
+            println!("Could not open device");
+            exit(1);
+        }
+    };
+
+    //Control transfer read request
+    let request_type: u8 = 0xa1;
+    let request: u8 = 0x01;
+    let value: u16 = 0x00;
+    let index: u16 = 0x00;
+    let mut buffer: [u8; MAXBUFSIZE] = [0x00; MAXBUFSIZE];
+
+    loop {
+        //First send an input request to the device then read whatever the device sends back
+        let bytes_read: usize =
+            match dhandle.read_control(request_type, request, value, index, &mut buffer, TIMEOUT) {
+                Ok(br) => br,
+                Err(err) => {
+                    println!("No bytes read! Error: {}", err);
+                    exit(2);
+                }
+            };
+        println!("Bytes read: {bytes_read}");
+
+        //lossy will turn every valid utf8 character into the appropriate symbol, while the invalid
+        //ones will be shown as this symbol: � (it returns a smart pointer)
+        let buf_to_string = String::from_utf8_lossy(&buffer);
+        println!("From device: {}", buf_to_string);
     }
 }
 
